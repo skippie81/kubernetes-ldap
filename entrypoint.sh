fc475af825a8b22f6837dbb6e6ee848d8322c3c7
Joeri Scapicchio <31068429+skippie81@users.noreply.github.com>
2017-12-30 22:40:25 +0100

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ then
   # get args from env vars
   env | egrep "KUBERNETES_LDAP_"
   ARGS=$( env | egrep "^KUBERNETES_LDAP_" | sed 's/^KUBERNETES_LDAP_/--/g' | sed 's/_/-/g' | tr '\n' ' ' )
-  echo "running: $_CMD $ARGS ${@:2}"
-  $_CMD $ARGS ${@:2}
+  echo "running: $_CMD $ARGS"
+  $_CMD $ARGS --logtostderr
 else
   $@
 fi
